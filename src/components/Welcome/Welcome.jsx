3e5bcb5e548044c09e418bd09c25643f39c6ba25
ushieru <uzielcocolan@gmail.com>
2021-12-18 09:34:42 -0600

    add templates

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,7 +3,7 @@ import styles from './Welcome.module.css'
 const Welcome = () => {
     return <>
         <div className={styles.mainContainer}>
-            <p className={styles.title}>Plantillas:</p>
+            <p className={styles.title}>Templates:</p>
             <div className={styles.templateContainer}>
                 <a href="/personal">
                     <img className={styles.imageTemplate} src="/personal-test.png" alt="personal-test" />
