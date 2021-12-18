acd62d7900b19658dd9e3fce3dc26ecfa0cb8c2c
ushieru <uzielcocolan@gmail.com>
2021-12-18 09:33:58 -0600

    add welcome page

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,21 @@
+import styles from './Welcome.module.css'
+
 const Welcome = () => {
     return <>
+        <div className={styles.mainContainer}>
+            <p className={styles.title}>Plantillas:</p>
+            <div className={styles.templateContainer}>
+                <a href="/personal">
+                    <img className={styles.imageTemplate} src="/personal-test.png" alt="personal-test" />
+                </a>
+                <a href="/company-double">
+                    <img className={styles.imageTemplate} src="/company-double-test.png" alt="company-double-test" />
+                </a>
+                <a href="/company-simple">
+                    <div style={{ display: 'inline-block', backgroundColor: 'gray' }} className={styles.imageTemplate} />
+                </a>
+            </div>
+        </div>
     </>
 }
 
